0f43ed60f424193b0a53ff4c89066e8292febaf8
Yar417 <y.negodenko@gmail.com>
2024-01-03 00:03:22 +0300

    PreDeploy commit

--- webapp/src/proj/settings.py
+++ webapp/src/proj/settings.py
@@ -6,16 +6,13 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['54.37.142.109', 'localhost', '0.0.0.0.0', '127.0.0.1']
 
 # Application definition
 
@@ -104,7 +101,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
